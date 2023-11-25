36e9a6a832d78ee58adac1b6ef21763a7803d8ac
Vamsi <rajavamsi05@gmail.com>
2023-11-25 13:20:18 +0530

    AutoPush

---
+++ 02-esc-sequence.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Welcome To Shell Scripting
+echo "This is day 1 of our shell scripting"
+
+# Escape Sequence characters
+
+#  \n : New Line 
+# \t : tab Space 
+
+echo -e "Line1\nLine2"
+echo -e  "Line3\tLine4"
+echo Line5
