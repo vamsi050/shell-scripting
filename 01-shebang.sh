4b562def6802f3c86436b5b8c7164b87b788b10a
Vamsi <rajavamsi05@gmail.com>
2023-11-25 11:00:36 +0530

    Added first program

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -1,1 +1,13 @@
-What is shebang notation?
+#!/bin/shell
+
+# Anything that starts with # is referred as a comment.
+# Comments are just for the purpose of humans to make to more verbose.
+# Shebang Notation
+
+# Shebang Notation : This tells the script with which interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first line of the script.
+
+# Apart from first line if you see anyline that starts with # that's referred as a comment. 
+
+echo Welcome To Shell Scripting
+echo "This is day 1 of our shell scripting"
