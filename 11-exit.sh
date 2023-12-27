29e44de66520d6dd081fcb2ebe6deffb0799d10c
Vamsi <rajavamsi05@gmail.com>
2023-12-27 09:04:36 +0530

    Auto Push

---
+++ 11-exit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# This is a single line comment 
+
+# Below is an example of multi line comment
+
+<<COMMENT
+echo "Cloud DevOps Training"
+echo "Shall Scripting"
+a=100
+b=300
+echo $a
+COMMENT 
+echo "Value of b is $b"
